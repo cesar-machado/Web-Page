49f9a82a5f0c0f0cb2d2aa0a8d566c4898038d43
César Machado <cesarmach19@gmail.com>
2021-09-01 11:55:32 -0300

    arrumando a reponsividade

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -1,46 +1,47 @@
-// import { useEffect, useState } from "react";
-// import ProjetosList from "../ProjetosList/ProjetosList";
+import { useEffect, useState } from "react";
+import { AiFillGithub, AiOutlineGlobal } from "react-icons/ai";
+import ProjetosList from "../ProjetosList/ProjetosList";
 import "./projetos.scss";
-// import { featuredPortfolio, webPortfolio, mobilePortfolio } from "../../data";
+import { featuredPortfolio, webPortfolio, mobilePortfolio } from "../../data";
 
 export default function Portfolio() {
-  // const [selected, setSelected] = useState("featured");
-  // const [data, setData] = useState([]);
-  // const list = [
-  //   {
-  //     id: "featured",
-  //     title: "Destaques",
-  //   },
-  //   {
-  //     id: "web",
-  //     title: "Web App",
-  //   },
-  //   {
-  //     id: "mobile",
-  //     title: "Aplicativos",
-  //   },
-  // ];
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Destaques",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Aplicativos",
+    },
+  ];
 
-  // useEffect(() => {
-  //   switch (selected) {
-  //     case "featured":
-  //       setData(featuredPortfolio);
-  //       break;
-  //     case "web":
-  //       setData(webPortfolio);
-  //       break;
-  //     case "mobile":
-  //       setData(mobilePortfolio);
-  //       break;
-  //     default:
-  //       setData(featuredPortfolio);
-  //   }
-  // }, [selected]);
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
 
   return (
     <section className="projetos" id="Projetos">
       <h1>Projetos</h1>
-      {/* <ul>
+      <ul>
         {list.map((item) => (
           <ProjetosList
             key={item.id}
@@ -56,10 +57,17 @@ export default function Portfolio() {
           <div className="item" key={d.title}>
             <img src={d.img} alt="" />
             <h3>{d.title}</h3>
+
+            <a href={d.url} target="_blank" rel="noreferrer" className="url">
+              <AiOutlineGlobal fontSize="30" />
+            </a>
+            <a href={d.urlGit} target="_blank" rel="noreferrer" className="git">
+              <AiFillGithub fontSize="30" />
+            </a>
           </div>
         ))}
-      </div> */}
-      <h3>Projetos em construção........</h3>
+      </div>
+      {/* <h3>Projetos em construção........</h3> */}
     </section>
   );
 }
