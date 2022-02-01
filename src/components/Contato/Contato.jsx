8b267c2de08d07c8828c4b0cc9a6a1f4f00f81b2
cesar-machado <cesarmach19@gmail.com>
2022-02-01 04:14:19 -0300

    Arrumando a responsividade do site

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -1,7 +1,7 @@
-import emailjs from "emailjs-com";
-import ImgContato from "../../assets/contato.png";
+import emailjs from 'emailjs-com';
+import ImgContato from '../../assets/contato.png';
 
-import "./contato.scss";
+import './contato.scss';
 
 import {
   FaLinkedin,
@@ -10,17 +10,17 @@ import {
   FaTwitterSquare,
   FaWhatsappSquare,
   FaGithubSquare,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 export default function Contato() {
   function sendEmail(e) {
     e.preventDefault();
 
     emailjs
-      .sendForm("gmail", "meu_site", e.target, "user_HWfAgjML3yqGNBG5cn3Cn")
+      .sendForm('gmail', 'meu_site', e.target, 'user_HWfAgjML3yqGNBG5cn3Cn')
       .then(
         (result) => {
-          alert("Sua mensagem foi enviada com sucesso.✅");
+          alert('Sua mensagem foi enviada com sucesso.✅');
         },
         (error) => {
           console.log(error.text);
@@ -29,120 +29,120 @@ export default function Contato() {
     e.target.reset();
   }
   return (
-    <section className="contato" id="Contato">
-      <div className="wrapper">
-        <div className="left">
-          <img src={ImgContato} alt="avatar" />
+    <section className='contato' id='Contato'>
+      <div className='wrapper'>
+        <div className='left'>
+          <img src={ImgContato} alt='avatar' />
           <h1>Entre em Contato</h1>
           <form onSubmit={sendEmail}>
-            <div className="inputs">
+            <div className='inputs'>
               <div>
                 <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Nome"
-                  name="name"
+                  type='text'
+                  className='form-control'
+                  placeholder='   Nome'
+                  name='name'
                 />
               </div>
               <div>
                 <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Endereço de Email"
-                  name="email"
+                  type='email'
+                  className='form-control'
+                  placeholder='   Endereço de Email'
+                  name='email'
                 />
               </div>
               <div>
                 <textarea
-                  className="form-control"
-                  id=""
-                  rows="5"
-                  placeholder="Mensagem"
-                  name="message"
+                  className='form-control'
+                  id=''
+                  rows='5'
+                  placeholder='   Mensagem'
+                  name='message'
                 ></textarea>
               </div>
               <div>
-                <button type="submit" className="btn">
+                <button type='submit' className='btn'>
                   Enviar
                 </button>
               </div>
             </div>
           </form>
         </div>
-        <div className="right">
-          <div className="sociais">
+        <div className='right'>
+          <div className='sociais'>
             <h2>Me siga ou converse comigo nas redes sociais</h2>
-            <div className="linkedin">
+            <div className='linkedin'>
               <a
-                href="https://www.linkedin.com/in/cesar-mach/"
-                target="_blank"
-                rel="noreferrer"
+                href='https://www.linkedin.com/in/cesar-mach/'
+                target='_blank'
+                rel='noreferrer'
               >
-                <FaLinkedin color="#0e76a8" fontSize="30px" className="icon" />
+                <FaLinkedin color='#0e76a8' fontSize='30px' className='icon' />
                 <p>cesar-mach</p>
               </a>
             </div>
-            <div className="instagram">
+            <div className='instagram'>
               <a
-                href="https://www.instagram.com/o.cesar.machado/"
-                target="_blank"
-                rel="noreferrer"
+                href='https://www.instagram.com/o.cesar.machado/'
+                target='_blank'
+                rel='noreferrer'
               >
                 <FaInstagramSquare
-                  color="#d6249f"
-                  fontSize="30px"
-                  className="icon"
+                  color='#d6249f'
+                  fontSize='30px'
+                  className='icon'
                 />
                 <p>o.cesar.machado</p>
               </a>
             </div>
-            <div className="twitter">
+            <div className='twitter'>
               <a
-                href="https://twitter.com/cesarmach30"
-                target="_blank"
-                rel="noreferrer"
+                href='https://twitter.com/cesarmach30'
+                target='_blank'
+                rel='noreferrer'
               >
                 <FaTwitterSquare
-                  color="#00aced"
-                  fontSize="30px"
-                  className="icon"
+                  color='#00aced'
+                  fontSize='30px'
+                  className='icon'
                 />
                 <p>cesarmach30</p>
               </a>
             </div>
-            <div className="github">
+            <div className='github'>
               <a
-                href="https://github.com/cesar-machado"
-                target="_blank"
-                rel="noreferrer"
+                href='https://github.com/cesar-machado'
+                target='_blank'
+                rel='noreferrer'
               >
                 <FaGithubSquare
-                  color="#171515"
-                  fontSize="30px"
-                  className="icon"
+                  color='#171515'
+                  fontSize='30px'
+                  className='icon'
                 />
                 <p>cesar-machado</p>
               </a>
             </div>
-            <div className="whatsApp">
+            <div className='whatsApp'>
               <a
-                href="https://api.whatsapp.com/send?phone=5511992812563"
-                target="_blank"
-                rel="noreferrer"
+                href='https://api.whatsapp.com/send?phone=5511992812563'
+                target='_blank'
+                rel='noreferrer'
               >
                 <FaWhatsappSquare
-                  color="#34af23"
-                  fontSize="30px"
-                  className="icon"
+                  color='#34af23'
+                  fontSize='30px'
+                  className='icon'
                 />
                 <p> +55 11 99281-2563</p>
               </a>
               <a
-                href="mailto:cesarmach19@gmail.com"
-                target="_blank"
-                rel="noreferrer"
+                href='mailto:cesarmach19@gmail.com'
+                target='_blank'
+                rel='noreferrer'
               >
-                <FaEnvelope color="#5436d8" fontSize="30px" className="icon" />
+                <FaEnvelope color='#5436d8' fontSize='30px' className='icon' />
                 <p>cesarmach19@gmail.com</p>
               </a>
             </div>
